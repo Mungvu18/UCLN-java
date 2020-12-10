ecfa2e80292f75cb843b53300099b235f9660407
MungC2010G2 <zhaiduong26z@gmail.com>
2020-12-10 15:13:53 +0700

    add file UCLN

---
+++ src/UCLN.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class UCLN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Moi ban nhap so a");
+        int a = sc.nextInt();
+        System.out.println("Moi ban nhap so b");
+        int b = sc.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if(a==0&&b==0){
+            System.out.println("Không có ước chung lớn nhất");
+        }
+        while (a!=b){
+            if(a>b){
+                a = a -b;
+            } else {
+                b = b - a;
+            }
+        }
+        System.out.println("Ước chung lớn nhất của a và b là " + a);
+    }
+}
